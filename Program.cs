323955c05da7d1050406f236ed16c012334dd7ab
laurenceQuentin <37290181+laurenceQuentin@users.noreply.github.com>
2022-11-17 20:40:17 +0100

    my second git commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace EsercizioLogFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            log.logger()
+;        }
+    }
+    public static class log {
+        public static DirectoryInfo Path = Path ;
+        public static string message="ciao!!";
+        public static DateTime curentData = DateTime.Now;
+        public static string myDirectory = Directory.GetCurrentDirectory();
+
+        public static void logger() {
+
+            //File.Create();
+            Console.WriteLine($"{message} hai aperto la cartella X alle {curentData}");
+        }
+
+        
+    }
+}
